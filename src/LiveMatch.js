8277fa8668550d3c6829275dbf6284faca4a605d
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-10-30 20:14:26 +0100

    Add LiveMatch Component

---
+++ src/LiveMatch.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class LiveMatch extends Component {
+  render() {
+    return (
+      <div className="LiveMatch">
+        <div className="LiveMatch-time">
+          <h2>08:00</h2>
+        </div>
+        <div className="LiveMatch-homeTeam">
+          FC Lucerne
+        </div>
+        <div className="LiveMatch-AwayTeam">
+          FC Lucerne
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LiveMatch;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LiveMatch from './LiveMatch';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <LiveMatch />,
   document.getElementById('root')
 );
