d5a4e974884c6a2a07746f76cff028daa25c21f9
Adrien Bigler <adrien.bigler@heig-vd.ch>
2016-11-02 17:48:32 +0100

    Live match time pattern
    - Need to be deeply tested

--- src/LiveMatch.js
+++ src/LiveMatch.js
@@ -11,9 +11,31 @@ class LiveMatch extends Component {
   }
 
   componentDidMount() {
-    console.log(this.props.time)
-    console.log(moment().locale())
-    console.log(moment(this.props.time).format("LT"))
+    
+  }
+
+  matchIsToday(matchDate){
+    var dateNow = Date.now();
+    return (moment(dateNow).diff(matchDate) == 0)
+  }
+  computeTime(matchDate){
+    var dateNow = Date.now();
+    var minutes = moment(dateNow).diff(matchDate, 'minutes')
+    if(minutes <= 45){
+      return moment(dateNow).diff(matchDate, 'minutes')
+    } else
+    if(minutes > 45 && minutes < 48){ // First half additional time (3')
+      return 45
+    } else
+    if(minutes >= 48 && minutes < 63){ // Half-time
+      return "MT"
+    } else
+    if(minutes >= 63 && minutes <= 108 ){ // Second half
+      return moment(dateNow).diff(matchDate, 'minutes')-18 // Substract 18 minutes (additional time + half time => 3 + 15)
+    } else
+    if(minutes > 108 && minutes <= 112 ){ // Half-time
+      return 90
+    }
   }
 
   render() {
@@ -23,7 +45,7 @@ class LiveMatch extends Component {
         <Flexbox flexDirection="row">
           <Flexbox flexDirection="column">
             <Flexbox className="time">{moment(this.props.time).format("HH:mm")}</Flexbox>
-            <Flexbox></Flexbox>
+            <Flexbox className="duration">{this.computeTime(this.props.time)}</Flexbox>
           </Flexbox>
           <Flexbox flexGrow={1} flexDirection="column" className="teams">
             <Flexbox>{this.props.homeTeam}</Flexbox>
@@ -37,6 +59,10 @@ class LiveMatch extends Component {
       </div>
     );
   }
+}
+
+function TimeBox(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
 export default LiveMatch;
